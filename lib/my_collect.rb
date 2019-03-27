e398b0543d85cd9bfd0db2c8a46b3165c0cc57d7
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-27 11:52:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  if block_given?
+  arr = 0
+    while arr.downcase do
+array_new = array.upcase
+  arr += 1
+    end 
+  yield array_new
+  end
+end
 
 
+array = [1, 20, 3, 4, 5, 6, 5, 4, 10]
+
+my_collect(array)
